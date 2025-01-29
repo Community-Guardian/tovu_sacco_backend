dc946d6183deed8ad1c9aa432b343b8ebc84d092
Nrad8394 <94044917+Nrad8394@users.noreply.github.com>
2025-01-29 15:34:22 +0300

    update

--- sacco/investments/views.py
+++ sacco/investments/views.py
@@ -1,6 +1,7 @@
-# views.py
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.db.models import Sum
 from .models import InvestmentType, Investment, InvestmentAccount, UserInvestment, Dividend
 from .serializers import InvestmentTypeSerializer, InvestmentSerializer, InvestmentAccountSerializer, UserInvestmentSerializer, DividendSerializer
 
@@ -12,17 +13,55 @@ class InvestmentViewSet(viewsets.ModelViewSet):
     queryset = Investment.objects.all()
     serializer_class = InvestmentSerializer
 
+    def get_queryset(self):
+        """
+        Limit the investments to those associated with the user's account if the user is a customer.
+        """
+        user = self.request.user
+        if user.role == 'customer':  # Assuming 'role' is a field in your user model
+            investment_ids = UserInvestment.objects.filter(account__account__user=user).values_list('investment__id', flat=True)
+            return self.queryset.filter(id__in=investment_ids)
+        return self.queryset
+
 class InvestmentAccountViewSet(viewsets.ModelViewSet):
     queryset = InvestmentAccount.objects.all()
     serializer_class = InvestmentAccountSerializer
+
+    def get_queryset(self):
+        """
+        Limit the investment account to the user's account if the user is a customer.
+        """
+        user = self.request.user
+        if user.role == 'customer':
+            return self.queryset.filter(account__user=user)
+        return self.queryset
 
 class UserInvestmentViewSet(viewsets.ModelViewSet):
     queryset = UserInvestment.objects.all()
     serializer_class = UserInvestmentSerializer
 
+    def get_queryset(self):
+        """
+        Limit user investments to the ones associated with the user's account if the user is a customer.
+        """
+        user = self.request.user
+        if user.role == 'customer':
+            return self.queryset.filter(account__account__user=user)
+        return self.queryset
+
 class DividendViewSet(viewsets.ModelViewSet):
     queryset = Dividend.objects.all()
     serializer_class = DividendSerializer
+
+    def get_queryset(self):
+        """
+        Limit the dividends to the ones related to the user's investment account if the user is a customer.
+        """
+        user = self.request.user
+        if user.role == 'customer':
+            investment_account_ids = InvestmentAccount.objects.filter(account__user=user).values_list('id', flat=True)
+            return self.queryset.filter(investment_account__in=investment_account_ids)
+        return self.queryset
 
     def create(self, request, *args, **kwargs):
         """
